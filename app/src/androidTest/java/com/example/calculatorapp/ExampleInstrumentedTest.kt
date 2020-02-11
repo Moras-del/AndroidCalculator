0494bb2d07c3e41872a9c7cf44b1b01aaac760b9
morek <morekszymon@wp.pl>
2020-02-11 19:29:06 +0100

    divider params

--- app/src/androidTest/java/com/example/calculatorapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/calculatorapp/ExampleInstrumentedTest.kt
@@ -1,21 +1,23 @@
 package com.example.calculatorapp
 
-import androidx.test.InstrumentationRegistry
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.filters.LargeTest
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Matchers.allOf
 
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 import org.junit.Rule
 
 /**
@@ -28,7 +30,7 @@ import org.junit.Rule
 class ExampleInstrumentedTest {
 
     @get:Rule
-    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+    val intentsTestRule: IntentsTestRule<MainActivity> = IntentsTestRule(MainActivity::class.java)
 
     @Test
     fun should_calculate_expression(){
@@ -36,7 +38,21 @@ class ExampleInstrumentedTest {
         val variable: String = "X=10, y=5"
         onView(withId(R.id.functionEditText)).perform(typeText(expression), closeSoftKeyboard())
         onView(withId(R.id.varEditText)).perform(typeText(variable), closeSoftKeyboard())
-        onView(withId(R.id.button)).perform(click())
+        onView(withId(R.id.resultbutton)).perform(click())
         onView(withId(R.id.resultTextView)).check(matches(withText("Wynik: 9.00")))
+    }
+
+    @Test
+    fun should_intent_to_chart(){
+        val expression: String = "sin(x)"
+        val range: String = "10"
+        onView(withId(R.id.functionEditText)).perform(typeText(expression), closeSoftKeyboard())
+        onView(withId(R.id.rangeEditText)).perform(typeText(range), closeSoftKeyboard())
+        onView(withId(R.id.makeChartButton)).perform(click())
+        intended(
+            allOf(
+                hasExtra(intentsTestRule.activity.getString(R.string.function_name), expression),
+                hasExtra(intentsTestRule.activity.getString(R.string.range_name), range),
+                toPackage("com.example.calculatorapp")))
     }
 }
--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -3,25 +3,16 @@ package com.example.calculatorapp
 import android.content.Context
 import android.content.Intent
 import android.os.*
-import android.text.Editable
 import androidx.appcompat.app.AppCompatActivity
-import android.util.Log
-import android.view.View
 import android.view.Window
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import com.anychart.AnyChart
-import com.anychart.chart.common.dataentry.DataEntry
-import com.anychart.chart.common.dataentry.ValueDataEntry
-import com.anychart.charts.Cartesian
-import net.objecthunter.exp4j.Expression
 import net.objecthunter.exp4j.ExpressionBuilder
-import org.w3c.dom.Text
-import java.io.Serializable
 import java.math.RoundingMode
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main)
-        val buttonCompute: Button       = findViewById(R.id.button)
+        val buttonCompute: Button       = findViewById(R.id.resultbutton)
         val buttonChart: Button         =       findViewById(R.id.makeChartButton)
         val resultTextView: TextView    =    findViewById(R.id.resultTextView)
         val rangeEditText: EditText     =     findViewById(R.id.rangeEditText)
@@ -39,9 +30,9 @@ class MainActivity : AppCompatActivity() {
 
         val v:Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
-        buttonCompute!!.setOnClickListener(){
-                val function: String = functionEditText?.text.toString().toLowerCase()
-                val variable = varEditText?.text.toString()
+        buttonCompute.setOnClickListener {
+                val function: String = functionEditText.text.toString()
+                val variable = varEditText.text.toString()
                                                 .replace(" ","")
                                                 .toLowerCase()
                                                 .split(",")
@@ -52,10 +43,10 @@ class MainActivity : AppCompatActivity() {
                     } else {
                         v.vibrate(500);
                     }
-                    if (function.equals("")) {
-                        functionEditText?.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake))
+                    if (function == "") {
+                        functionEditText.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake))
                     } else if (variable[0] == "") {
-                        varEditText?.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake))
+                        varEditText.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake))
                     }
                 } else {
                     try {
@@ -68,7 +59,7 @@ class MainActivity : AppCompatActivity() {
                             expression!!.setVariable(n[0].toString(), n.substring(2).toDouble())
                         }
                         val result: Double = expression!!.evaluate()
-                        resultTextView?.text = "Wynik: " + result.toBigDecimal().setScale(
+                        resultTextView.text = "Wynik: " + result.toBigDecimal().setScale(
                             2,
                             RoundingMode.HALF_EVEN
                         ).toString()
@@ -98,8 +89,8 @@ class MainActivity : AppCompatActivity() {
                 }
             } else {
                 val intent = Intent(this@MainActivity, ChartActivity::class.java) //INTENT DO WYKRESU
-                intent.putExtra("FUNCTION", function)
-                intent.putExtra("RANGE", range)
+                intent.putExtra(getString(R.string.function_name), function)
+                intent.putExtra(getString(R.string.range_name), range)
                 startActivity(intent)
             }
         }
